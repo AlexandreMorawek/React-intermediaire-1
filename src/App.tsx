a1efbb4b5d624b8b0566f1fe2ad7fd47e175bb4d
AlexandreMorawek <alexandre.morawek@gmail.com>
2025-04-10 16:39:31 +0200

    loading from API directly

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
 
 import './App.css'
 import EmployeeCard from './components/EmployeeCard'
-import { useState } from 'react';
-
-const sampleEmployee = {
-  name: {
-    first: "Charlie",
-    last: "Thompson",
-  },
-  email: "charlie.thompson@example.com",
-  picture: {
-    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
-  },
-};
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [employee, setEmployee] = useState(sampleEmployee);
+  const [employee, setEmployee] = useState(null);
+
+  useEffect(() => {
+    getEmployee();
+  }, []);
+  
   const getEmployee = () => {
     // Send the request
     fetch("https://randomuser.me/api?nat=en")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data)
         setEmployee(data.results[0]);
       });
   };
 
   return (
     <>
-      <EmployeeCard employee={employee}/>
+      {employee ? (<EmployeeCard employee={employee} />)
+       : (<p>Loading employee...</p>)}
+    
       <button type="button" onClick={getEmployee}>Get employee</button>
     </>
   )
